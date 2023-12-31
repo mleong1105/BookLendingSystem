adf7439a7e8156299bc74313b7bc928805bbfed8
LAPTOP-A247K2F8\PC <chanmanleong5@gmail.com>
2023-12-31 09:54:59 +0800

    update citation machine

--- src/main/java/com/example/booklending/citation/CitateMachineComponent.java
+++ src/main/java/com/example/booklending/citation/CitateMachineComponent.java
@@ -7,7 +7,13 @@ import com.google.firebase.database.ValueEventListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Component
 public class CitateMachineComponent {
@@ -15,12 +21,15 @@ public class CitateMachineComponent {
     @Autowired
     private DatabaseReference databaseReference;
 
+    //Function 1:
+    //Adding book to citation list function
+
     public Boolean addBookToCitationList(Map<String, Object> bookData, String userId) {
         try {
             DatabaseReference userRef = databaseReference.child("users").child(userId).child("citationList");
-            Boolean duplicateStatus = addBookForExistingList(bookData, userRef);
+            CompletableFuture<Boolean> duplicateStatus = addBookForExistingList(bookData, userRef);
 
-            return duplicateStatus;
+            return duplicateStatus.get();
 
         } catch (Exception e) {
             // Handle exceptions as needed
@@ -29,31 +38,43 @@ public class CitateMachineComponent {
         }
     }
 
-    private boolean duplicate = false;
-
-    private boolean addBookForExistingList(Map<String, Object> bookData, DatabaseReference userRef) {
+    private CompletableFuture<Boolean> addBookForExistingList(Map<String, Object> bookData, DatabaseReference userRef) {
         try {
             // Assuming your bookData contains necessary fields like title, author, etc.
-            String bookTitle = (String) bookData.get("title");
-            String bookAuthor = (String) bookData.get("author");
+            String bookAuthor = (String) bookData.get("Author");
+            String bookYear = (String) bookData.get("Year");
+            String bookTitle = (String) bookData.get("Title");
+            String bookPublisher = (String) bookData.get("Publisher");
+            String bookSource = (String) bookData.get("Source");
             String bookuid = (String) bookData.get("uid");
 
             DatabaseReference bookchildRef = userRef.child(bookuid);
+
+            CompletableFuture<Boolean> duplicateFuture = new CompletableFuture<>();
             bookchildRef.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.exists()) {
-                        duplicate = true;
-                        System.out.println("Item already exists in Citation List");
-                    } else {
+                    boolean duplicate = dataSnapshot.exists();
+                    duplicateFuture.complete(duplicate);
+
+                    if (!duplicate) {
                         Map<String, Object> bookObject = Map.of(
-                            "title", bookTitle,
-                            "author", bookAuthor
+                            "Author", bookAuthor,
+                            "Year", bookYear,
+                            "Title", bookTitle,
+                            "Publisher", bookPublisher,
+                            "Source", bookSource
                         );
 
                         // Set the book data under the bookUid
-                        bookchildRef.setValueAsync(bookObject);
+                        bookchildRef.setValue(bookObject, (databaseError, databaseReference) -> {
+                            if (databaseError != null) {
+                                duplicateFuture.completeExceptionally(databaseError.toException());
+                            }
+                        });
                         System.out.println("Book added successfully");
+                    } else {
+                        System.out.println("Item has existed in CitationList");
                     }
                 }
 
@@ -64,15 +85,173 @@ public class CitateMachineComponent {
                 }
             });
 
-            Boolean returnDuplicate = duplicate;
-            duplicate = false;
-
-            return returnDuplicate;
+            return duplicateFuture;
 
         } catch (Exception e) {
             // Handle exceptions as needed
             e.printStackTrace();
             throw new RuntimeException("Failed to add book for existing user in citationList", e);
+        }
+    }
+
+    //Function 2:
+    //Get citation book list function
+
+    public CompletableFuture<Map<String, Object>> getCitationBookList(String userId) {
+        CompletableFuture<Map<String, Object>> futureResult = new CompletableFuture<>();
+
+        try {
+            DatabaseReference userRef = databaseReference.child("users").child(userId).child("citationList");
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Map<String, Object> result = new HashMap<>();
+
+                    if (dataSnapshot.exists()) {
+                        result.put("status", true);
+                        System.out.println("Citation Book List Exist");
+
+                        List<Map<String, Object>> citationList = new ArrayList<>();
+                        for (DataSnapshot bookSnapshot : dataSnapshot.getChildren()) {
+                            String bookName = bookSnapshot.getKey();
+                            Map<String, Object> bookData = new HashMap<>();
+                            for (DataSnapshot propertySnapshot : bookSnapshot.getChildren()) {
+                                String propertyName = propertySnapshot.getKey();
+                                Object propertyValue = propertySnapshot.getValue();
+                                bookData.put(propertyName, propertyValue);
+                            }
+                            bookData.put("uid", bookName);
+                            citationList.add(bookData);
+                        }
+
+                        result.put("citationList", citationList);
+                        System.out.println("BookList get successfully");
+                    } else {
+                        result.put("status", false);
+                        System.out.println("Citation Book List Not Exist");
+                    }
+
+                    futureResult.complete(result);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Handle onCancelled event
+                    futureResult.completeExceptionally(new RuntimeException("Failed to check citationList", error.toException()));
+                }
+            });
+
+        } catch (Exception e) {
+            // Handle exceptions as needed
+            e.printStackTrace();
+            throw new RuntimeException("Failed to add book to citationList", e);
+        }
+
+        return futureResult;
+    }
+
+    //Function 3:
+    //Citate the book list and get the reference according to APA format
+
+    public CompletableFuture<List<String>> generateReferenceList(Map<String, Object> bookListRequest) {
+        String formatName = (String) bookListRequest.get("formatName");
+        List<Map<String, Object>> citateList = (List<Map<String, Object>>) bookListRequest.get("citationList");
+    
+        DatabaseReference formatRef = databaseReference.child("citationFormat").child(formatName);
+    
+        CompletableFuture<List<String>> referenceListFuture = new CompletableFuture<>();
+    
+        formatRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String formatString = snapshot.child("formatString").getValue(String.class);
+                    List<Map<String, Object>> formatFields = (List<Map<String, Object>>) snapshot.child("field").getValue();
+                    formatFields.sort(Comparator.comparingInt(field -> {
+                        Object sequenceObject = field.get("sequence");
+                        if (sequenceObject instanceof Number) {
+                            return ((Number) sequenceObject).intValue();
+                        } else {
+                            throw new IllegalArgumentException("Sequence value is not a number");
+                        }
+                    }));
+    
+                    List<CompletableFuture<String>> referenceFuture = citateList.stream()
+                            .map(book -> {
+                                Object[] fieldValues = formatFields.stream()
+                                        .map(field -> {
+                                            String fieldName = (String) field.get("name");
+                                            return book.containsKey(fieldName) ? book.get(fieldName).toString() : "";
+                                        })
+                                        .toArray();
+    
+                                return CompletableFuture.completedFuture(String.format(formatString, fieldValues));
+                            })
+                            .collect(Collectors.toList());
+
+                    CompletableFuture<Void> allOf = CompletableFuture.allOf(
+                            referenceFuture.toArray(new CompletableFuture[0])
+                    );
+    
+                    allOf.thenAccept(ignoredVoid -> {
+                        List<String> references = referenceFuture.stream()
+                                .map(CompletableFuture::join)
+                                .collect(Collectors.toList());
+                        referenceListFuture.complete(references);
+                    });
+                } else {
+                    referenceListFuture.completeExceptionally(new RuntimeException("Citation format not found: " + formatName));
+                }
+            }
+    
+            @Override
+            public void onCancelled(DatabaseError error) {
+                referenceListFuture.completeExceptionally(new RuntimeException("Failed to fetch citation format: " + error.getMessage(), error.toException()));
+            }
+        });
+    
+        return referenceListFuture;
+    }    
+
+    //Function 4
+    //Remove book from Citation Book List
+
+    public CompletableFuture<Boolean> removeBookfromCitationList(String bookuid, String userId) {
+        try {
+            DatabaseReference userRef = databaseReference.child("users").child(userId).child("citationList").child(bookuid);
+            CompletableFuture<Boolean> bookExistsFuture = new CompletableFuture<>();
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    boolean bookExists = dataSnapshot.exists();
+                    bookExistsFuture.complete(bookExists);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    bookExistsFuture.completeExceptionally(error.toException());
+                }
+            });
+
+            Boolean bookExists = bookExistsFuture.get();
+            if (bookExists) {
+                CompletableFuture<Void> removeFuture = new CompletableFuture<>();
+                userRef.removeValue((databaseError, databaseReference) -> {
+                    if (databaseError == null) {
+                        removeFuture.complete(null);
+                    } else {
+                        removeFuture.completeExceptionally(databaseError.toException());
+                    }
+                });
+    
+                removeFuture.get();
+            }
+            return CompletableFuture.completedFuture(bookExists);
+
+        } catch (Exception e) {
+            // Handle exceptions as needed
+            e.printStackTrace();
+            throw new RuntimeException("Failed to remove book from citationList", e);
         }
     }
 }
--- src/main/java/com/example/booklending/citation/CitateMachineController.java
+++ src/main/java/com/example/booklending/citation/CitateMachineController.java
@@ -1,7 +1,10 @@
 package com.example.booklending.citation;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,16 +20,103 @@ public class CitateMachineController {
     @Autowired
     private CitateMachineComponent citateMachineComponent;
 
+    //hardcode uid for testing
+    private String uid = "bNrmYjgcJsgYHm2yQDPwV4r0lHk2";
+
     // Endpoint to add a book to the citation list
     @PostMapping("/api/addCitationBook")
-    public ResponseEntity<Map<String, Object>> addBookToCitationList(@RequestBody Map<String, Object> bookData, Authentication authentication) {
+    // Suppose to pass Authentication obj which store the session token that contains the uid
+    public ResponseEntity<Map<String, Object>> addCitationBook(@RequestBody Map<String, Object> bookData) {
         try {
-            // Assuming you have a method in CitateMachineComponent to add a book
-            Boolean duplicateStatus = citateMachineComponent.addBookToCitationList(bookData, authentication.getName());
-
+            // Boolean duplicateStatus = citateMachineComponent.addBookToCitationList(bookData, authentication.getName());
+            Boolean duplicateStatus = citateMachineComponent.addBookToCitationList(bookData, uid);
             Map<String, Object> response = new HashMap<>();
             response.put("message", !duplicateStatus ? "Book added successfully" : "Item has existed in CitationList");
             response.put("duplicateStatus", duplicateStatus);
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Failed to add the book: " + e.getMessage());
+            errorResponse.put("duplicateStatus", false);
+
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/api/getCitationBookList")
+    // Suppose to pass Authentication obj which store the session token that contains the uid
+    public CompletableFuture<ResponseEntity<Map<String, Object>>> getCitationBookList() {
+        // return citateMachineComponent.getCitationBookList(authentication.getName())
+        return citateMachineComponent.getCitationBookList(uid)
+        .thenApply(result -> {
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", (Boolean) result.get("status") ? "Citation Book List get successfully" : "Citation Book List does not exist");
+            response.put("existStatus", result.get("status"));
+
+            if((Boolean) result.get("status")) {
+                response.put("citationList", result.get("citationList"));
+            }
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        })
+        .exceptionally(exception -> {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Failed to get the citation list: " + exception.getMessage());
+            errorResponse.put("duplicateStatus", false);
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        });
+    }
+
+    @PostMapping("/api/generateReferenceList")
+    // Suppose to pass Authentication obj which store the session token that contains the uid
+    public ResponseEntity<Map<String, Object>> generateReferenceList(@RequestBody String citateFormat) {
+        try {
+            // CompletableFuture<Map<String, Object>> citationBookListFuture = citateMachineComponent.getCitationBookList(authentication.getName());
+            CompletableFuture<Map<String, Object>> citationBookListFuture = citateMachineComponent.getCitationBookList(uid);
+
+            Map<String, Object> citationBookListResult = citationBookListFuture.get();
+            Boolean citationListExist = (Boolean) citationBookListResult.get("status");
+
+            if (citationListExist) {
+                Map<String, Object> bookListRequest = new HashMap<>();
+                List<String> citationList = (List<String>) citationBookListResult.get("citationList");
+                bookListRequest.put("formatName", citateFormat);
+                bookListRequest.put("citationList", citationList);
+
+                CompletableFuture<List<String>> referenceListFuture = citateMachineComponent.generateReferenceList(bookListRequest);
+
+                List<String> references = referenceListFuture.get();
+
+                Map<String, Object> response = new HashMap<>();
+                response.put("status", citationListExist);
+                response.put("message", "Reference List generated successfully");
+                response.put("referenceList", references);
+
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            } else {
+                Map<String, Object> errorResponse = new HashMap<>();
+                errorResponse.put("status", citationListExist);
+                errorResponse.put("message", "Citation Book List does not exist");
+                return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Failed to generate references: " + e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/api/removeCitationBook")
+    // Suppose to pass Authentication obj which store the session token that contains the uid
+    public ResponseEntity<Map<String, Object>> removeCitationBook(@RequestBody String bookuid) {
+        try {
+            // Boolean duplicateStatus = citateMachineComponent.addBookToCitationList(bookData, authentication.getName());
+            CompletableFuture<Boolean> existStatus = citateMachineComponent.removeBookfromCitationList(bookuid, uid);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", existStatus.get() ? "Book removed successfully" : "Item does not existed in CitationList");
+            response.put("duplicateStatus", existStatus);
 
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
